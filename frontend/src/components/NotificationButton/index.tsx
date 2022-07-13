116c994eead196bf920a3f09c9ec49c4920c954b
placebofx94 <samuelbartolomedi1994@gmail.com>
2022-07-13 15:19:28 -0300

    first component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import iconeNotificar from '../../assets/img/NotificationIcon.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={iconeNotificar} alt="Notificar" />
+        </div>
+
+    )
+}
+
+export default NotificationButton
